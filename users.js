62a71457f89f47c934b96b04c653222b07b59603
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-08-09 23:21:08 +0000

    pseudodatabase users.js file working. Need to fix login/logout logic in header

---
+++ users.js
@@ -0,0 +1,16 @@
+const generateRandomString = require('./modules/generateRandomString');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  userDb: {},
+  createNewUser: function(email, password) {
+    let newIdKey = generateRandomString(6);
+    let hashedPassword = bcrypt.hashSync(password, 10);
+
+    this.userDb[newIdKey] = {
+      id: newIdKey,
+      email: email,
+      password: hashedPassword
+    };
+  }
+};
